8943b7fdd8225dad57d65e34e8bdf38cde062cdd
Cooper Hsiung <cooperhsiung@gmail.com>
2018-02-28 14:39:37 +0800

    up

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var pathToRegexp = require('path-to-regexp');
 const routed = module.exports = (path, opts = {}) => (target, name, descriptor) => {
     const fn = descriptor.value;
-    descriptor.value = function (ctx, next) {
+    descriptor.value = (ctx, next) => {
         opts.method = (opts.method || 'GET').toUpperCase();
         if (pathToRegexp(path).test(ctx.path) && opts.method === ctx.method) {
             return fn.call(target, ctx, next)
